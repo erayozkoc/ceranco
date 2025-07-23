0f36dbec7118a7b4bfd470ff195c6185ecc99a83
erayozkoc <erayozkoc@gmail.com>
2025-07-23 22:57:21 +0300

    add gtm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,11 +32,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="tr" className="overflow-x-hidden">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MV8JQQ7V');`,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" crossOrigin="anonymous" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="alternate" hrefLang="tr" href="https://ceranco.com.tr/" />
       </head>
+
       <body className={`${poppins.className} overflow-x-hidden bg-brand`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MV8JQQ7V"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <WhatsAppButton />
